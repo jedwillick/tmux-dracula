0af2e14cdba0939c70f74006de1a38f65c0a711a
jedwillick <jwillick@me.com>
2022-11-27 21:00:00 +1000

    feat: Customise spotify-tui segment to my liking

--- scripts/spotify-tui.sh
+++ scripts/spotify-tui.sh
@@ -2,22 +2,35 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
-main()
-{
+# https://stackoverflow.com/a/3352015
+trim() {
+  local var="$*"
+  # remove leading whitespace characters
+  var="${var#"${var%%[![:space:]]*}"}"
+  # remove trailing whitespace characters
+  var="${var%"${var##*[![:space:]]}"}"
+  printf '%s' "$var"
+}
+
+main() {
   # storing the refresh rate in the variable RATE, default is 5
   RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
 
-  if ! command -v spt &> /dev/null
-  then
+  if ! command -v spt &> /dev/null; then
     exit 1
   fi
 
-  spotify_playback=$(spt playback)
-  echo ${spotify_playback}
+  raw=$(spt playback --format "%s %t;%a")
 
+  [ -z "$raw" ] && return
+
+  title=$(trim "$(echo "$raw" | cut -d';' -f1 | cut -d'-' -f1)")
+  artist=$(trim "$(echo "$raw" | cut -d';' -f2 | cut -d',' -f1)")
+  full="$title - $artist"
+  echo "${full}"
 }
 
 # run the main driver
